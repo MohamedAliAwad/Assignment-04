c436b1e853db95e721003a36d24c745030a64968
Mohamed <mohamed.ali.awad2018@gmail.com>
2025-07-23 02:53:13 +0300

    assignment 04

--- Assignment 04/Program.cs
+++ Assignment 04/Program.cs
@@ -1,52 +1,152 @@
-﻿#region Q01
+﻿
+#region Q01
+
+//Console.WriteLine("Please enter a number");
+//int num = int.TryParse(Console.ReadLine(), out int number)?  number : 0;
+
+//for(int i = 1; i <= num; i++)
+//{
+//    Console.Write($"{i}, ");
+//}
 
 
 #endregion
 
 
 #region Q02
+//Console.WriteLine("Please enter a number");
+//int num = int.TryParse(Console.ReadLine(), out int number) ? number : 0;
 
+//for (int i = 1; i <= 12; i++)
+//{
+//    Console.WriteLine($"{num} * {i} = {i*num}");
+//}
 
 #endregion
 
 
 #region Q03
+//Console.WriteLine("Please enter a number");
+//int num = int.TryParse(Console.ReadLine(), out int number) ? number : 0;
 
+//for (int i = 1; i <= num; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        Console.Write($"{i}, ");
+//    }
+//}
 
 #endregion
 
 
 #region Q04
-
+//Console.WriteLine("Please enter a number");
+//int num = int.TryParse(Console.ReadLine(), out int number) ? number : 0;
+//int result = 1;
+//for (int i = num; i <= num; i--)
+//{
+//    if(i == 0) 
+//        {
+//        break;
+//    }
+//    result *= num;
+//}
+//Console.WriteLine(result);
 
 #endregion
 
 
 #region Q05
+//Console.WriteLine("Please enter Mark 1");
+//int mark1 = int.TryParse(Console.ReadLine(), out int number) ? number : 0;
+//Console.WriteLine("Please enter Mark 2");
+//int mark2 = int.TryParse(Console.ReadLine(), out int number1) ? number1 : 0;
+//Console.WriteLine("Please enter Mark 3");
+//int mark3 = int.TryParse(Console.ReadLine(), out int number2) ? number2 : 0;
+//Console.WriteLine("Please enter Mark 4");
+//int mark4 = int.TryParse(Console.ReadLine(), out int number3) ? number3 : 0;
+//Console.WriteLine("Please enter Mark 5");
+//int mark5 = int.TryParse(Console.ReadLine(), out int number4) ? number4 : 0;
+//decimal totalMarks = mark1 + mark2 + mark3 + mark4 + mark5;
+//decimal averageMarks = totalMarks / 5;
+//decimal percentageMarks = (totalMarks / 500) * 100;
+
+//for (int i = 1; i <= 5; i++)
+//{
+//    if (i == 1)
+//    {
+//        Console.WriteLine($"Mark {i} is {mark1}");
+//    }
+//    else if (i == 2)
+//    {
+//        Console.WriteLine($"Mark {i} is {mark2}");
+//    }
+//    else if (i == 3)
+//    {
+//        Console.WriteLine($"Mark {i} is {mark3}");
+//    }
+//    else if (i == 4)
+//    {
+//        Console.WriteLine($"Mark {i} is {mark4}");
+//    }
+//    else if (i == 5)
+//    {
+//        Console.WriteLine($"Mark {i} is {mark5}");
+//    }
+//}
+
+
+//Console.WriteLine($"The Total of the marks is {totalMarks}");
+//Console.WriteLine($"The average of the marks is {averageMarks}");
+//Console.WriteLine($"The Percentage of the marks is {percentageMarks}%");
+
+
+
 
 
 #endregion
 
 
 #region Q06
-
+//Console.WriteLine("Please enter string to reverse");
+//string input = Console.ReadLine();
+//Console.WriteLine(input.Reverse().ToArray());
 
 #endregion
 
 
 #region Q07
+//Console.WriteLine("Please enter a number to reverse");
+//int num = int.TryParse(Console.ReadLine(), out int number) ? number : 0;
+//string reversed = num.ToString();
+
+//Console.WriteLine(reversed.Reverse().ToArray());
+
 
 
 #endregion
 
 
 #region Q08
+//Console.WriteLine("Please enter a number");
+//int num = int.TryParse(Console.ReadLine(), out int number) ? number : 0;
 
+//for (int i = 2; i < num; i++)
+//{
+//    if (num % i != 0)
+//    {
+//        Console.Write($"{i} ,");
+
+//    }
+//}
 
 #endregion
 
 
 #region Q09
+
+
 
 
 #endregion
@@ -55,10 +155,32 @@
 #region Q10
 
 
+
+
+
 #endregion
 
 
 #region Q11
+//Console.WriteLine("Please enter a number");
+//int num = int.TryParse(Console.ReadLine(), out int number) ? number : 0;
+//for (int i = 0; i < num; i++)
+//{
+//    for (int j = 0; j < num; j++)
+//    {     //  1    1
+//           // 2    2
+//        if (i == j)
+//        {
+//            Console.Write("1 ");
+//        }
+//        else
+//        {
+//            Console.Write("0 ");
+//        }
+//    }
+//    Console.WriteLine();
+//}
+
 
 
 #endregion
